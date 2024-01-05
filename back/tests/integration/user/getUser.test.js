89281134f0ead960f54ac2d3446639ea4683e8e5
Carlos Antonio Sales de Oliveira Junior <carlosamtoni@gmail.com>
2024-01-05 17:12:32 -0300

    test: adicionando testes para a rota get /user/:id

--- back/tests/integration/user/getUser.test.js
+++ back/tests/integration/user/getUser.test.js
@@ -8,14 +8,14 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Get /user', function () {
+describe('Get /user/:id', function () {
 
   afterEach(sinon.restore);
 
-  it('Get users', async function () {
+  it('Get user/:id', async function () {
     sinon.stub(connection, 'execute').resolves([[
       {
-        id: 1,
+        id: 2,
         name: 'Carlos',
         email: 'carlos@gmail.com',
         type_id: 2,
@@ -27,32 +27,17 @@ describe('Get /user', function () {
         number: 120,
         complement: 'Casa',
         people_id: 2
-      },
-      {
-        id: 2,
-        name: 'Amanda',
-        email: 'amanda@gmail.com',
-        type_id: 2,
-        cep: 55940000,
-        uf: 'RJ',
-        city: 'Niteroi',
-        district: 'Centro',
-        street: 'Rua Louro',
-        number: 110,
-        complement: 'Casa',
-        people_id: 3
-      }
-    ]]);
+      }]]);
 
     const response = await chai.request(app)
-      .get('/user')
+      .get('/user/2')
       .set('authorization', '52aedf85d7542e28');
 
     expect(response.status).to.equal(200);
-    expect(response.body.length).to.equal(2);
+    expect(response.body.length).to.equal(1);
     expect(response.body).to.deep.equal([
       {
-        id: 1,
+        id: 2,
         name: 'Carlos',
         email: 'carlos@gmail.com',
         type_id: 2,
@@ -64,41 +49,34 @@ describe('Get /user', function () {
         number: 120,
         complement: 'Casa',
         people_id: 2
-      },
-      {
-        id: 2,
-        name: 'Amanda',
-        email: 'amanda@gmail.com',
-        type_id: 2,
-        cep: 55940000,
-        uf: 'RJ',
-        city: 'Niteroi',
-        district: 'Centro',
-        street: 'Rua Louro',
-        number: 110,
-        complement: 'Casa',
-        people_id: 3
-      }
-    ]);
+      }]);
   });
 
-  it('Get user com o token não encontrado', async function () {
+  it('Get user/:id onde id é uma string', async function () {
+    const response = await chai.request(app)
+      .get('/user/2dssd')
+      .set('authorization', '52aedf85d7542e28');
+
+    expect(response.status).to.equal(404);
+    expect(response.body.message).to.equal('O parametro "id" deve ser um número inteiro maior ou igual a 0');
+  });
+
+  it('Get user:id com o token não encontrado', async function () {
 
     const response = await chai.request(app)
-      .get('/user');
+      .get('/user/2');
 
     expect(response.status).to.equal(401);
     expect(response.body.message).to.equal('Token não encontrado');
   });
 
-  it('Get user com toke inválido', async function () {
+  it('Get user/:id com toke inválido', async function () {
 
     const response = await chai.request(app)
-      .get('/user')
+      .get('/user/2')
       .set('authorization', '52aedf85d7542e');
 
     expect(response.status).to.equal(401);
     expect(response.body.message).to.equal('Token inválido');
   });
 });
-
